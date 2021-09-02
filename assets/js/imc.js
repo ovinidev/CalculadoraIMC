a7c54d2f4ebc67f803c69cb898d98149e55c4a8d
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-02 08:59:44 -0300

    ajusting title and fav icon

--- assets/js/imc.js
+++ assets/js/imc.js
@@ -1,71 +1,69 @@
 var $btn = document.querySelector("#btn");
 
-$btn.addEventListener("click", pegavalor);
+$btn.addEventListener("click", getValue);
 
-function pegavalor(){
-  if(validation() === true){
+function getValue() {
+  if (validation() === true) {
     var $altura = document.querySelector("#altura").value;
     var $peso = document.querySelector("#peso").value;
-  
+
     $altura = parseFloat($altura);
     $peso = parseFloat($peso);
-  
+
     const indice = imc_calc($altura, $peso);
 
     imc_resultado(indice);
 
-    window.scrollTo(0,document.body.scrollHeight);
-    
-  }else{
+    window.scrollTo(0, document.body.scrollHeight);
+
+  } else {
     alert("Digite os valores em todos os campos");
   }
 }
 
+function imc_calc(altura, peso) {
 
-function imc_calc(altura, peso){
-
-  var imc = peso / (altura*altura);
+  var imc = peso / (altura * altura);
 
   return imc;
 }
 
+function imc_resultado(indice) {
+  if (indice < 16) return change("default", "#3e70d4", "Não existe um resultado para valores de IMC abaixo de 16.");
 
-function imc_resultado(indice){
-  if(indice < 16) return  change("default", "#3e70d4", "Não existe um resultado para valores de IMC abaixo de 16.");
-
-  if(indice >= 16 && indice <= 16.99) 
+  if (indice >= 16 && indice <= 16.99)
     return change("red", "#f60a0a", "Muito abaixo do peso");
 
-  if(indice >= 17 && indice <= 18.44) 
+  if (indice >= 17 && indice <= 18.44)
     return change("yellow", "#e2fa08", "Abaixo do peso");
 
-  if(indice >= 18.5 && indice <= 24.99) 
+  if (indice >= 18.5 && indice <= 24.99)
     return change("green", "#20f00d", "Peso normal");
 
-  if(indice >= 25 && indice <= 29.99) 
+  if (indice >= 25 && indice <= 29.99)
     return change("yellow", "#e2fa08", "Acima do peso");
 
-  if(indice >= 30 && indice <= 34.99) 
+  if (indice >= 30 && indice <= 34.99)
     return change("red", "#f60a0a", "Obesidade Grau I");
 
-  if(indice >= 35 && indice <= 40) 
+  if (indice >= 35 && indice <= 40)
     return change("red", "#f60a0a", "Obesidade Grau II");
 
-  if(indice > 40) 
+  if (indice > 40)
     return change("red", "#f60a0a", "Obesidade Grau III");
 
 }
 
-function validation(){
+function validation() {
   const $altura = document.querySelector("#altura").value;
   const $peso = document.querySelector("#peso").value;
 
-
-  if(!$altura || !$peso){
-    return false;
-  }else{
+  if ($altura || $peso) {
     return true;
+  } else {
+    return false;
   }
+
 }
 
 
