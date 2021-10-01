315698ff2d01fbb236ec8baad2013c6fa89f559d
Jorgevvs <jorgevinicius9@gmail.com>
2021-10-01 13:40:57 -0300

    small changes on collor

--- assets/js/switchMode.js
+++ assets/js/switchMode.js
@@ -4,6 +4,8 @@ switchButton.addEventListener("click", switchTheme)
 function switchTheme(){
     document.querySelector("#moon").classList.toggle("themeOff")
     document.querySelector("#sun").classList.toggle("themeOff")
-    
+    document.querySelector(".btn").classList.toggle("lightModeBtn")
+    document.querySelector("h1").classList.toggle("titleLight")
+    document.querySelector(".imc_menu").classList.toggle("imc_menu_Light")
 
 }
